40b384123842aa09053db0d0803d86321de66e2f
Norman Maurer <norman_maurer@apple.com>
2021-01-15 11:41:24 +0100

    Fix site

--- .github/scripts/build_qemu_image.sh
+++ .github/scripts/build_qemu_image.sh
@@ -7,5 +7,5 @@ fi
 
 FILENAME=Fedora-Cloud-Base-33-1.2.x86_64.qcow2
 wget -q https://download.fedoraproject.org/pub/fedora/linux/releases/33/Cloud/x86_64/images/$FILENAME
-qemu-img create -f qcow2 -b $FILENAME $1 2G
+qemu-img create -f qcow2 -b $FILENAME $1 20G
 genisoimage -output $2 -volid cidata -joliet -rock ./.github/config/user-data ./.github/config/meta-data
