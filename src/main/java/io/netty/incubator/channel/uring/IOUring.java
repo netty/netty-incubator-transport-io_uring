netty/netty-incubator-transport-io_uring

commit 7cbe31f05f6951ef6810a40b820a827ba93f9779

Parsing Linux kernel version to prevent JVM crashes (#97)
Motivation:

To prevent JVM crashes we should first parse the kernel version to make sure that the kernel is at least 5.9

Modifications:

Add kernel version check to the static scoping in IOUring

Result:

Prevent JVM crashes on older kernel

src/main/java/io/netty/incubator/channel/uring/IOUring.java
@@ -24,10 +24,14 @@ public final class IOUring {
 
     static {
         Throwable cause = null;
+        String kernelVersion = Native.kernelVersion();
 
         if (SystemPropertyUtil.getBoolean("io.netty.transport.noNative", false)) {
             cause = new UnsupportedOperationException(
                     "Native transport was explicit disabled with -Dio.netty.transport.noNative=true");
+        } else if (!Native.checkKernelVersion(kernelVersion)) {
+            cause = new UnsupportedOperationException(
+                    "you need at least kernel version 5.9, current kernel version: " + kernelVersion);
         } else {
             Throwable unsafeCause = PlatformDependent.getUnsafeUnavailabilityCause();
             if (unsafeCause == null) {

src/main/c/netty_io_uring_native.c
@@ -60,6 +60,7 @@
 #include <poll.h>
 // Needed for UDP_SEGMENT
 #include <netinet/udp.h>
+#include <sys/utsname.h>
 
 // Allow to compile on systems with older kernels.
 #ifndef UDP_SEGMENT
@@ -169,6 +170,14 @@ static jint netty_io_uring_enter(JNIEnv *env, jclass class1, jint ring_fd, jint 
         }
     } while ((err = errno) == EINTR);
     return -err;
+}
+
+static jstring netty_io_uring_kernel_version(JNIEnv* env, jclass clazz) {
+    struct utsname u;
+    uname(&u);
+
+    jstring result = (*env)->NewStringUTF(env, u.release);
+    return result;
 }
 
 static jint netty_epoll_native_blocking_event_fd(JNIEnv* env, jclass clazz) {
@@ -604,7 +613,7 @@ static const JNINativeMethod method_table[] = {
     {"eventFdWrite", "(IJ)V", (void *) netty_io_uring_eventFdWrite },
     {"registerUnix", "()I", (void *) netty_io_uring_registerUnix },
     {"cmsghdrData", "(J)J", (void *) netty_io_uring_cmsghdrData},
-
+    {"kernelVersion", "()Ljava/lang/String;", (void *) netty_io_uring_kernel_version },
 };
 static const jint method_table_size =
     sizeof(method_table) / sizeof(method_table[0]);

src/main/java/io/netty/incubator/channel/uring/Native.java
@@ -195,6 +195,36 @@ final class Native {
         }
     }
 
+    static boolean checkKernelVersion(String kernelVersion) {
+        String[] versionComponents = kernelVersion.split("\\.");
+        if (versionComponents.length < 3) {
+            return false;
+        }
+
+        int major;
+        try {
+            major = Integer.parseInt(versionComponents[0]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        if (major <= 4) {
+            return false;
+        }
+        if (major > 5) {
+            return true;
+        }
+
+        int minor;
+        try {
+            minor = Integer.parseInt(versionComponents[1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return minor >= 9;
+    }
+
     private static native boolean ioUringProbe(int ringFd, int[] ios);
     private static native long[][] ioUringSetup(int entries);
 
@@ -219,6 +249,8 @@ final class Native {
     private static native int registerUnix();
 
     static native long cmsghdrData(long hdrAddr);
+
+    static native String kernelVersion();
 
     private Native() {
         // utility

src/test/java/io/netty/incubator/channel/uring/NativeTest.java
@@ -297,4 +297,16 @@ public class NativeTest {
             ringBuffer.close();
         }
     }
+
+    @Test
+    public void parsingKernelVersionTest() {
+        assertTrue(Native.checkKernelVersion("10.11.123"));
+        assertFalse(Native.checkKernelVersion("5.8.1-23"));
+        assertTrue(Native.checkKernelVersion("5.100.1-1"));
+        assertTrue(Native.checkKernelVersion("5.9.1-1"));
+        assertTrue(Native.checkKernelVersion("5.9.100-1"));
+        assertFalse(Native.checkKernelVersion("5.5.67"));
+        assertFalse(Native.checkKernelVersion("5.5.32"));
+        assertFalse(Native.checkKernelVersion("4.16.20"));
+    }
 }

commit 2ff1dfac34673590270847ec7ddfd0526c7b06aa

Ensure IOUring#isAvailable does not throw Exception
Motivation:

When invoking IOUring#isAvailable the exception below is thrown. This is a regression made with #97

java.lang.ExceptionInInitializerError
	at io.netty.incubator.channel.uring.IOUring.<clinit>(IOUring.java:27)
	at java.lang.Class.forName0(Native Method)
	at java.lang.Class.forName(Class.java:264)
	at reactor.netty.resources.DefaultLoopIOUring.<clinit>(DefaultLoopIOUring.java:95)
	at reactor.netty.resources.DefaultLoopNativeDetector.<clinit>(DefaultLoopNativeDetector.java:32)
	at reactor.netty.resources.LoopResources.hasNativeSupport(LoopResources.java:270)
...
Caused by: java.lang.IllegalStateException: Only supported on Linux
	at io.netty.incubator.channel.uring.Native.loadNativeLibrary(Native.java:277)
	at io.netty.incubator.channel.uring.Native.<clinit>(Native.java:69)
	... 106 more

This is related to https://github.com/reactor/reactor-netty/actions/runs/992135575

Modifications:

- Introduce "global" try/catch that will handle all exceptions thrown by
io.netty.incubator.channel.uring.Native and initialises IOUring#UNAVAILABILITY_CAUSE.
Thus this will ensure IOUring#isAvailable does not throw Exception

Result:

Ensure IOUring#isAvailable does not throw Exception, but give information
whether this functionality is supported or not

src/main/java/io/netty/incubator/channel/uring/IOUring.java
@@ -24,35 +24,37 @@ public final class IOUring {
 
     static {
         Throwable cause = null;
-        String kernelVersion = Native.kernelVersion();
+        try {
+            String kernelVersion = Native.kernelVersion();
 
-        if (SystemPropertyUtil.getBoolean("io.netty.transport.noNative", false)) {
-            cause = new UnsupportedOperationException(
-                    "Native transport was explicit disabled with -Dio.netty.transport.noNative=true");
-        } else if (!Native.checkKernelVersion(kernelVersion)) {
-            cause = new UnsupportedOperationException(
-                    "you need at least kernel version 5.9, current kernel version: " + kernelVersion);
-        } else {
-            Throwable unsafeCause = PlatformDependent.getUnsafeUnavailabilityCause();
-            if (unsafeCause == null) {
-                RingBuffer ringBuffer = null;
-                try {
-                    ringBuffer = Native.createRingBuffer();
-                    Native.checkAllIOSupported(ringBuffer.fd());
-                } catch (Throwable t) {
-                    cause = t;
-                } finally {
-                    if (ringBuffer != null) {
-                        try {
-                            ringBuffer.close();
-                        } catch (Exception ignore) {
-                            // ignore
+            if (SystemPropertyUtil.getBoolean("io.netty.transport.noNative", false)) {
+                cause = new UnsupportedOperationException(
+                        "Native transport was explicit disabled with -Dio.netty.transport.noNative=true");
+            } else if (!Native.checkKernelVersion(kernelVersion)) {
+                cause = new UnsupportedOperationException(
+                        "you need at least kernel version 5.9, current kernel version: " + kernelVersion);
+            } else {
+                Throwable unsafeCause = PlatformDependent.getUnsafeUnavailabilityCause();
+                if (unsafeCause == null) {
+                    RingBuffer ringBuffer = null;
+                    try {
+                        ringBuffer = Native.createRingBuffer();
+                        Native.checkAllIOSupported(ringBuffer.fd());
+                    } finally {
+                        if (ringBuffer != null) {
+                            try {
+                                ringBuffer.close();
+                            } catch (Exception ignore) {
+                                // ignore
+                            }
                         }
                     }
+                } else {
+                    cause = new UnsupportedOperationException("Unsafe is not supported", unsafeCause);
                 }
-            } else {
-                cause = new UnsupportedOperationException("Unsafe is not supported", unsafeCause);
             }
+        } catch (Throwable t) {
+            cause = t;
         }
 
         UNAVAILABILITY_CAUSE = cause;